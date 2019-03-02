22fbb9175631cd01aa74485becf9576581609490
drsysmts <30734741+drsysmts@users.noreply.github.com>
2019-03-02 16:59:32 +0100

    Add files via upload

---
+++ ps0.py
@@ -0,0 +1,9 @@
+# work
+import math
+
+x =int(input("Enter number x: "))   # x input number int
+y =int(input("Enter number y: "))   #  y input number int
+# print resulat = x**y and log(x)  base 2
+
+print("X**Y  = {}\nlog(x) = {} ".format(x**y, math.log(x,2)))
+
